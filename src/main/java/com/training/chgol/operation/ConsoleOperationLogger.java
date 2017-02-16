8a4b4cf6d77b10d9b8d13a0f195851a9b220ba3a
Christian Goltz <christiangoltz@hotmail.com>
2017-02-16 15:39:17 +0100

    AOP version

--- src/main/java/com/training/chgol/operation/ConsoleOperationLogger.java
+++ src/main/java/com/training/chgol/operation/ConsoleOperationLogger.java
@@ -1,31 +1,22 @@
 package com.training.chgol.operation;
 
-import org.aspectj.lang.annotation.*;
 import com.training.chgol.BankException;
 
-@Aspect
 public class ConsoleOperationLogger {
 
     private static final String SEPARATOR = "####################################################################";
 
-    @Pointcut("execution(void com.training.chgol.service.AccountsService.process(..)) && args(operation)")
-    public void processOperation(Operation operation) {
-    }
-
-    @Before("processOperation(operation)")
     public void beforeOperation(Operation operation) {
         System.out.println(SEPARATOR);
         System.out.println(operation);
     }
 
-    @AfterReturning("processOperation(operation)")
-    public void onSuccess(Operation operation) {
-        System.out.format("Status: success \n%s\n", SEPARATOR);
+    public  void onSuccess(Operation operation) {
+        System.out.format("Status: success\n%s\n", SEPARATOR);
     }
 
-    @AfterThrowing(value = "processOperation(operation)", throwing = "ex")
     public void onException(Operation operation, BankException ex) {
-        System.out.format("Status: exception (%s) \n%s\n", ex.getClass().getSimpleName(), SEPARATOR);
+        System.out.format("Status: exception (%s)\n%s\n", ex.getClass().getSimpleName(), SEPARATOR);
     }
 
 }
--- src/main/java/com/training/chgol/service/AccountsService.java
+++ src/main/java/com/training/chgol/service/AccountsService.java
@@ -3,12 +3,10 @@ package com.training.chgol.service;
 import com.training.chgol.entity.Account;
 import com.training.chgol.operation.Operation;
 import com.training.chgol.service.repository.AccountsRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Transactional
 public class AccountsService {
 
     private AccountsRepository accountsRepository;
